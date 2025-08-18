315bba0afcb3f47557bac9e2e29546182173e67a
Qareeb Nazimudeen <qareebnazimudeen@Qareebs-MacBook-Air.local>
2025-08-18 01:33:16 -0400

    try and fix bad dployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ukrooze/', 
+  base: '/ukrooze/',
   plugins: [
     react(),
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
 })
